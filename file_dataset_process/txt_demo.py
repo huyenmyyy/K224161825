ff88f4dcb62b6f1534b0ef53000b1c37162a37d4
huyenmyyy <huyenmy.bt319@gmail.com>
2025-01-14 22:49:41 +0700

    first commit

---
+++ file_dataset_process/txt_demo.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df=pd.read_csv("../Dataset/SalesTransactions.txt",
+                encoding='utf-8', dtype='unicode',
+                sep='\t',
+                low_memory=False)
+print(df)
+
+
